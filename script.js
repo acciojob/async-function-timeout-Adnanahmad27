c453e623d26fc33fc93a0e463b25d3b20ae8a6af
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-23 16:15:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const form = document.querySelector('form');
+const output = document.getElementById('output');
+const btn = document.getElementById('btn');
+
+async function p2(text , delay){
+    return new Promise((resolve, reject) => {
+        setTimeout( () => {
+            resolve(text);
+        } , delay*1000);
+    });
+}
+
+btn.addEventListener('click' , async () => {
+    let text = document.getElementById('text').value;
+    let delay = Number(document.getElementById('delay').value);
+
+    let result = await p2(text , delay);
+    output.innerText = result;
+});
+
+ 
